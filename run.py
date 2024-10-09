ad5fa403141d4eaddf04ef5142149ab0d221ee2d
RieseM204 <riese.marshall204@gmail.com>
2024-10-09 10:54:59 -0500

    set up boilerplate for run.py

---
+++ run.py
@@ -0,0 +1,14 @@
+import math
+import os
+import numpy as np
+from scipy.signal import savgol_filter
+import matplotlib.pyplot as plt
+import pandas as pd
+
+def main() -> None:
+    """Main function"""
+
+    raise NotImplementedError
+
+if __name__ == "__main__":
+    main()
